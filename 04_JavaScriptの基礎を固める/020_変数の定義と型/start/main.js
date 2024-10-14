84c1aa13ce4e9a1366216562f7523925bbd696ea
yd-03 <3nu4ydye57@gmail.com>
2024-10-15 08:59:23 +0900

    Done: 6-56

---
+++ 04_JavaScriptの基礎を固める/020_変数の定義と型/start/main.js
@@ -0,0 +1,17 @@
+// var: 基本的に使わない
+// let: 再代入可能
+// const: 再代入不可能
+
+let name = "Tom";
+name = "Tim";
+console.log("name:", name);
+
+// Number, String, Boolean, Undefined, Null, Symbol
+
+// 動的型付け言語: 変数の型を宣言時に指定しない
+let variable = "str";
+variable = 12;
+variable = false;
+variable = undefined;
+
+console.log("variable:", variable);
