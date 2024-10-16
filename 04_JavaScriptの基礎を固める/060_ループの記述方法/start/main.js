836e63a554cff4f6d0d4baa4e5ebb6641b0e7650
yd-03 <3nu4ydye57@gmail.com>
2024-10-16 09:03:32 +0900

    Done: 6-61

---
+++ 04_JavaScriptの基礎を固める/060_ループの記述方法/start/main.js
@@ -0,0 +1,9 @@
+const array = [1, 2, 3, 4, 5, 6];
+
+//　i = 0: 初期化処理
+// i < 5: 条件式
+// i = i + 1: 更新処理
+for (let i = 0; i < array.length; i++) {
+  //   console.log(array[i]);
+  console.log(i);
+}
