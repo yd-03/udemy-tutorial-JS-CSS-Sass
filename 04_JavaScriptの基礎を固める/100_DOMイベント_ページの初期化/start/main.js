b1e1f090731e3274e24d04aaf2491e1c1dd88deb
yd-03 <3nu4ydye57@gmail.com>
2024-10-20 13:46:27 +0900

    Done: 6-73

--- 04_JavaScriptの基礎を固める/100_DOMイベント_ページの初期化/start/main.js
+++ 04_JavaScriptの基礎を固める/100_DOMイベント_ページの初期化/start/main.js
@@ -1,10 +1,11 @@
-const dcl = document.querySelector('.dcl');
-const load = document.querySelector('.load');
+const dcl = document.querySelector(".dcl");
+const load = document.querySelector(".load");
 
+// DOMContentLoaded: HTMLの読み込みが完了した時点で発火（画像などのリソースの読み込みを待たない）
 document.addEventListener("DOMContentLoaded", function () {
-    dcl.classList.add('done');
+  dcl.classList.add("done");
 });
-
+// load: 画像などの全てのリソースの読み込みが完了した時点で発火
 window.addEventListener("load", function () {
-    load.classList.add('done');
+  load.classList.add("done");
 });
