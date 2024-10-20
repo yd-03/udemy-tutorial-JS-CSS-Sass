869eb6325eb9d28f58151e6fa75456945570b7aa
yd-03 <3nu4ydye57@gmail.com>
2024-10-20 14:37:49 +0900

    Done: 6-75

--- 04_JavaScriptの基礎を固める/111_classを用いてコード再利用性を高めよう/start/main.js
+++ 04_JavaScriptの基礎を固める/111_classを用いてコード再利用性を高めよう/start/main.js
@@ -1,22 +1,29 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const el = document.querySelector('.animate-title');
-    const el2 = document.querySelector('.animate-title-2');
-    const str = el.innerHTML.trim().split("");
-    const str2 = el2.innerHTML.trim().split("");
-    
-    // let concatStr = '';
-
-    // for(let c of str) {
-    //     c = c.replace(/\s+/, '&nbsp;');
-    //     concatStr += `<span class="char">${c}</span>`;
-    // }
-
-    el.innerHTML = str.reduce((acc, curr) => {
-        curr = curr.replace(/\s+/, '&nbsp;');
-        return `${acc}<span class="char">${curr}</span>`;
-    }, "");
-    el2.innerHTML = str2.reduce((acc, curr) => {
-        curr = curr.replace(/\s+/, '&nbsp;');
-        return `${acc}<span class="char">${curr}</span>`;
-    }, "");
+document.addEventListener("DOMContentLoaded", () => {
+  const ta = new TextAnimation(".animate-title");
+  const ta2 = new TextAnimation(".animate-title-2");
+  setTimeout(() => {
+    ta.animate();
+    ta2.animate();
+  }, 1000);
 });
+
+class TextAnimation {
+  // コンストラクター関数: インスタンスを生成するときに実行される関数
+  constructor(el) {
+    // this: インスタンス自身を指す
+    this.el = document.querySelector(el);
+    this.chars = this.el.innerHTML.trim().split("");
+    this.el.innerHTML = this._splitText();
+  }
+  // プライベートメソッド: クラス内でしか使えないメソッド
+  _splitText() {
+    return this.chars.reduce((acc, curr) => {
+      curr = curr.replace(/\s+/, "&nbsp;");
+      return `${acc}<span class="char">${curr}</span>`;
+    }, "");
+  }
+  // パブリックメソッド: クラス外からでも使えるメソッド
+  animate() {
+    this.el.classList.toggle("inview");
+  }
+}
