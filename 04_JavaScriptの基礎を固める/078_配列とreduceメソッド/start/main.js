ffcbb8a551c27a83c2b5ef744c940801a66fea0c
yd-03 <3nu4ydye57@gmail.com>
2024-10-20 12:55:57 +0900

    Done: 6-69

--- 04_JavaScriptの基礎を固める/078_配列とreduceメソッド/start/main.js
+++ 04_JavaScriptの基礎を固める/078_配列とreduceメソッド/start/main.js
@@ -1,1 +1,10 @@
-const arry = [1,2,3,4,5];
+const arry = [1, 2, 3, 4, 5];
+
+// reduceメソッド: 配列の各要素に対して処理を行い、その結果を累積していく
+// reduceメソッドの引数にはコールバック関数を渡す
+// 第一引数には累積値（accumulator）が入る
+// 第二引数には配列要素（currentValue）が入る
+arry.reduce(function (accu, curr) {
+  console.log(accu, curr);
+  return accu + curr;
+});
