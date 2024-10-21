aebef94993e486ae2bc2bf0c0ed512f9bea1c20f
yd-03 <3nu4ydye57@gmail.com>
2024-10-22 08:26:23 +0900

    Done: 6-79

--- 04_JavaScriptの基礎を固める/115_thisとbind/start/main.js
+++ 04_JavaScriptの基礎を固める/115_thisとbind/start/main.js
@@ -1,13 +1,22 @@
 const obj = {
-    first_name: 'Mafia',
-    last_name: 'Code',
-    printFullName: function () {
+  first_name: "Mafia",
+  last_name: "Code",
+  printFullName: function () {
+    // オブジェクトのメソッド内でのthisはそのオブジェクトを指す
+    console.log(this);
+    // 下で定義している関数内で、thisの中身をobjを参照するようにする
+    const _that = this;
+    window.setTimeout(
+      function () {
+        // 関数内でのthisはwindowオブジェクトを指す
+        //   console.log(this);
+        // thisはobjを指す
+        //   console.log(_that);
         console.log(this);
-
-        window.setTimeout(function () {
-            console.log(this);
-        });
-    }
-}
+        // bind()を使うことで、関数内でのthisを指定したオブジェクトにすることができる
+      }.bind(this)
+    );
+  },
+};
 
 obj.printFullName();
