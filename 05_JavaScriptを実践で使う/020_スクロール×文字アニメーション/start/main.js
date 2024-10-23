ee0f909ad428a492d404d33daefac5bf6212ffc2
yd-03 <3nu4ydye57@gmail.com>
2024-10-23 23:08:12 +0900

    Done: 7-86

--- 05_JavaScriptを実践で使う/020_スクロール×文字アニメーション/start/main.js
+++ 05_JavaScriptを実践で使う/020_スクロール×文字アニメーション/start/main.js
@@ -1,44 +1,27 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const ta = new TextAnimation('.animate-title');
-    ta.animate();
-});
+document.addEventListener("DOMContentLoaded", function () {
+  // インターセクションオブザーバーを使ってスクロールに合わせて要素を表示する
+  const els = document.querySelectorAll(".animate-title"); // 監視対象の要素を取得
 
-// text-animation.jsに以下のコードをカット＆ペースト
-// してファイル分割をしましょう。
-class TextAnimation {
-    constructor(el) {
-        this.DOM = {};
-        this.DOM.el = document.querySelector(el);
-        this.chars = this.DOM.el.innerHTML.trim().split("");
-        this.DOM.el.innerHTML = this._splitText();
-    }
-    _splitText() {
-        return this.chars.reduce((acc, curr) => {
-            curr = curr.replace(/\s+/, '&nbsp;');
-            return `${acc}<span class="char">${curr}</span>`;
-        }, "");
-    }
-    animate() {
-        this.DOM.el.classList.toggle('inview');
-    }
-}
-class TweenTextAnimation extends TextAnimation {
-    constructor(el) {
-        super(el);
-        this.DOM.chars = this.DOM.el.querySelectorAll('.char');
-    }
-    
-    animate() {
-        this.DOM.el.classList.add('inview');
-        this.DOM.chars.forEach((c, i) => {
-            // TweenMax.toでも同じ意味。バージョン３以降はgsapの使用を推奨している。
-            gsap.to(c, .6, {
-                ease: Back.easeOut,
-                delay: i * .05,
-                startAt: { y: '-50%', opacity: 0},
-                y: '0%',
-                opacity: 1
-            });
-        });
-    }
-}
+  // コールバック関数を定義
+  const cb = function (entries, observer) {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const ta = new TextAnimation(".animate-title");
+        ta.animate();
+        observer.unobserve(entry.target);
+      } else {
+      }
+    });
+    //   alert("intersecting");
+  };
+
+  // インターセクションオブザーバーの設定
+  const options = {
+    root: null, //ビューポートをルート要素とする
+    rootMargin: "-300px 0px", // ビューポートのマージンを設定
+    threshold: [0, 0.5, 1], // 閾値を設定
+  };
+
+  const io = new IntersectionObserver(cb, options); // コールバック関数を設定
+  els.forEach((el) => io.observe(el)); // 監視を開始
+});
