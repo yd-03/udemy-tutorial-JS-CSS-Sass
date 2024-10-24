5a757c843870ad24f61a80d81ea1f424f590a36e
yd-03 <3nu4ydye57@gmail.com>
2024-10-24 09:04:46 +0900

    Done: 7-87

--- 05_JavaScriptを実践で使う/020_スクロール×文字アニメーション/start/main.js
+++ 05_JavaScriptを実践で使う/020_スクロール×文字アニメーション/start/main.js
@@ -1,25 +1,28 @@
 document.addEventListener("DOMContentLoaded", function () {
   // インターセクションオブザーバーを使ってスクロールに合わせて要素を表示する
+  // クラス名 animate-title が付与された全ての要素を NodeList として取得する
   const els = document.querySelectorAll(".animate-title"); // 監視対象の要素を取得
 
   // コールバック関数を定義
   const cb = function (entries, observer) {
     entries.forEach((entry) => {
+      // 監視対象の要素がビューポートに交差した（表示領域に入った）かどうかを判定
       if (entry.isIntersecting) {
-        const ta = new TextAnimation(".animate-title");
+        // スクロールで要素が表示された時に実行する処理（アニメーション）
+        const ta = new TextAnimation(entry.target);
         ta.animate();
+        // 一度要素が表示された後、その要素の監視を解除する
         observer.unobserve(entry.target);
       } else {
       }
     });
-    //   alert("intersecting");
   };
 
   // インターセクションオブザーバーの設定
   const options = {
-    root: null, //ビューポートをルート要素とする
-    rootMargin: "-300px 0px", // ビューポートのマージンを設定
-    threshold: [0, 0.5, 1], // 閾値を設定
+    root: null, //ビューポート（ブラウザの画面内表示領域）をルート要素とする
+    rootMargin: "0px", // ビューポートのマージンを設定
+    threshold: 0, // 閾値を設定
   };
 
   const io = new IntersectionObserver(cb, options); // コールバック関数を設定
--- 05_JavaScriptを実践で使う/020_スクロール×文字アニメーション/start/text-animation.js
+++ 05_JavaScriptを実践で使う/020_スクロール×文字アニメーション/start/text-animation.js
@@ -1,22 +1,21 @@
 class TextAnimation {
   constructor(el) {
     this.DOM = {};
-    if (el instanceof HTMLElement) {
-      this.DOM.el = el;
-    } else {
-      this.DOM.el = document.querySelector(el);
-    }
-    this.DOM.el = document.querySelector(el);
+    // elがHTMLElementの場合はそのまま、そうでない場合はquerySelectorで取得する
+    this.DOM.el = el instanceof HTMLElement ? el : document.querySelector(el);
     this.chars = this.DOM.el.innerHTML.trim().split("");
     this.DOM.el.innerHTML = this._splitText();
   }
   _splitText() {
     return this.chars.reduce((acc, curr) => {
+      // 空白文字を非改行スペースに変換
       curr = curr.replace(/\s+/, "&nbsp;");
       return `${acc}<span class="char">${curr}</span>`;
     }, "");
   }
   animate() {
+    // クラスが既に要素に存在していればそのクラスを削除し、存在していなければ追加する（トグル: 切り替える）
+    // アニメーションを繰り返したり、要素の表示・非表示を切り替える際に使用する
     this.DOM.el.classList.toggle("inview");
   }
 }
@@ -27,9 +26,13 @@ class TweenTextAnimation extends TextAnimation {
   }
 
   animate() {
+    // もしそのクラスがすでに存在していても、再度追加しようとはせず、クラスがただ存在する状態を保つ
     this.DOM.el.classList.add("inview");
+    // iはインデックス番号、.charの要素の数だけ繰り返す
     this.DOM.chars.forEach((c, i) => {
       // TweenMax.toでも同じ意味。バージョン３以降はgsapの使用を推奨している。
+      // GSAP（GreenSock Animation Platform）というアニメーションライブラリの関数で、要素の状態をアニメーションで変化させるために使用する
+      // gsap.to(要素, アニメーションの時間, アニメーションの設定)
       gsap.to(c, 0.6, {
         ease: Back.easeOut,
         delay: i * 0.05,
